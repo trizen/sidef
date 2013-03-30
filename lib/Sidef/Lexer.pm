d27e7fa70a17d2b5c4521a2a5b5b5b4dc1d42e0f
Trizen <trizenx@gmail.com>
2013-03-30 23:56:44 +0200

    Progress on lexer.

--- lib/Sidef/Lexer.pm
+++ lib/Sidef/Lexer.pm
@@ -1,5 +1,5 @@
 
-use 5.010;
+use 5.014;
 use strict;
 use warnings;
 
@@ -14,7 +14,7 @@ package Sidef::Lexer;
 use Sidef::Base;
 
 sub make_esc_delim {
-    if ( $_[0] ne '\\' ) {
+    if ($_[0] ne '\\') {
         my $delim = quotemeta shift;
         return qr{$delim([^$delim\\]*+(?>\\.|[^$delim\\]+)*+)$delim}s;
     }
@@ -37,10 +37,39 @@ sub new {
     my $cbracket    = 0;
     my $parentheses = 0;
 
+    sub get_method_name {
+        my %opt = @_;
+
+        state $operators_re = do {
+            local $" = q{|};
+
+            my @operators = map { quotemeta } qw(
+
+              && || // ** << >> ==
+              / + - * % ^ & | :  =
+
+              );
+
+            qr{(@operators)};
+        };
+
+        given ($opt{'code'}) {
+            when (/\G(\w+)/gc) {
+                return $1, pos;
+            }
+            when (m{\G$operators_re}gc) {
+                return $1, pos;
+            }
+            default {
+                die "Invalid method name, at line $line.\n";
+            }
+        }
+    }
+
     sub parse_expr {
         my (%opt) = @_;
 
-        given ( $opt{code} ) {
+        given ($opt{code}) {
             {
                 when (/\G#.*/gc) {
                     redo;
@@ -58,7 +87,7 @@ sub new {
                     }
                     continue;
                 }
-                when (/\G;/gc) {
+                when (/\G;/gc || /\G\z/gc) {
                     return undef, pos;
                 }
                 when (/\G$double_quote/gc) {
@@ -91,7 +120,7 @@ sub new {
         my $ref = $struct{main} //= [];
         my $class = 'main';
 
-        given ( $opt{code} ) {
+        given ($opt{code}) {
             {
                 when (/\Gclass\h+/gc) {
                     when (/\G"(.*?)"/gc) {
@@ -108,41 +137,69 @@ sub new {
                 when (/\G\z/gc) {
                     return \%struct;
                 }
-
                 when (/\G->/gc) {
 
-                    my $method_name;
-                    if (/\G(\w+)/gc) {
-                        $method_name = $1;
-                    }
-                    elsif (m{\G([-+/*%])}gc) {
-                        $method_name = $1;
-                    }
+                    my ($method_name, $pos) = get_method_name(code => substr($_, pos));
+                    pos($_) = $pos + pos;
+
+                    #die $method_name;
+
+                    push @{$struct{$class}[-1]{call}}, {name => $method_name,};
+
+                    redo;
+
+                    #if (/\G(\w+)/gc) {
+                    #     $method_name = $1;
+                    #}
+                    #elsif (m{\G([-+/*%])}gc) {
+                    #    $method_name = $1;
+                    #}
 
                     #++$#{$struct{$class}[-1]{call}};
                     #$struct{$class}[-1]{call} =
 
-                    my ( $expr, $pos ) = parse_expr
+                    #my ( $expr, $pos ) = parse_expr(code => substr($_, pos));
 
                 }
 
+                #while(/\G\h*,/gc){
+                #        my($obj, $pos) = parse_expr( code => substr($_, pos) );
+                #        pos($_) = $pos + pos;
+
+                # push as argument
+                # }
+
                 when (/\G/gc) {
-                    my $expr = substr( $_, pos($_) );
-                    my ( $obj, $pos ) = parse_expr( code => $expr );
-                    pos($_) = $pos + pos($_);
+                    my $expr = substr($_, pos);
+                    my ($obj, $pos) = parse_expr(code => $expr);
+                    pos($_) = $pos + pos;
 
-                    ++$ref;
-                   # $ref->[-1]
+                    if (defined $obj) {
 
-                      #++$#{$struct{$class}};
-                      #$struct{$class}[-1]{self} = $obj;
+                        push @{$struct{$class}}, {self => $obj,};
 
-                      #redo;
+                        redo;
+
+                    }
+                    else {
+                        # die "Undefined object, at line $line.\n";
+                        redo;
+                    }
+
+                    # ++$ref;
+                    # $ref->[-1]
+
+                    #++$#{$struct{$class}};
+                    #$struct{$class}[-1]{self} = $obj;
+
+                    #redo;
 
                     #push @{$struct{$class}}
                 }
             }
         }
+
+        return \%struct;
 
     }
 
@@ -209,8 +266,11 @@ my $code = <<'CODE';
 
 "string"->lc->uc->print;
 
-12/ ( 63/(2) /(3) )->to_s->print;
+#12/ ( 63/(2) /(3) )->to_s->print;
 
 CODE
 
-parse_script( code => $code );
+my $struct = parse_script(code => $code);
+
+use Data::Dumper;
+print Dumper $struct;
