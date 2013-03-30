badba53df19f8487de95d253b7886f0139fd9e67
Trizen <trizenx@gmail.com>
2013-03-31 01:30:29 +0200

    Lexer is working.

--- lib/Sidef/Lexer.pm
+++ lib/Sidef/Lexer.pm
@@ -37,23 +37,35 @@ sub new {
     my $cbracket    = 0;
     my $parentheses = 0;
 
+    state $operators_re = do {
+        local $" = q{|};
+
+        my @operators = map { quotemeta } qw(
+
+          && || // ** << >> ==
+          / + - * % ^ & | :  =
+
+          );
+
+        qr{(@operators)};
+    };
+
+    sub syntax_error {
+        my %opt = @_;
+        die "Syntax error near: --->",
+          substr($opt{'code'}, $opt{'pos'}, index($opt{'code'}, "\n", $opt{'pos'}) - $opt{'pos'}),
+          "<--- at line $line.\n";
+    }
+
     sub get_method_name {
         my %opt = @_;
 
-        state $operators_re = do {
-            local $" = q{|};
-
-            my @operators = map { quotemeta } qw(
-
-              && || // ** << >> ==
-              / + - * % ^ & | :  =
-
-              );
-
-            qr{(@operators)};
-        };
-
         given ($opt{'code'}) {
+
+            if (/\G/gc && defined(my $pos = parse_whitespace(code => substr($_, pos)))) {
+                pos($_) = $pos + pos($_);
+            }
+
             when (/\G(\w+)/gc) {
                 return $1, pos;
             }
@@ -66,7 +78,7 @@ sub new {
         }
     }
 
-    sub parse_expr {
+    sub parse_whitespace {
         my (%opt) = @_;
 
         given ($opt{code}) {
@@ -85,8 +97,25 @@ sub new {
                     when (/\G\v+/gc) {
                         redo;
                     }
-                    continue;
                 }
+                default {
+                    return pos;
+                }
+            }
+        }
+
+        return;
+    }
+
+    sub parse_expr {
+        my (%opt) = @_;
+
+        given ($opt{code}) {
+            {
+                if (/\G/gc && defined(my $pos = parse_whitespace(code => substr($_, pos)))) {
+                    pos($_) = $pos + pos($_);
+                }
+
                 when (/\G;/gc || /\G\z/gc) {
                     return undef, pos;
                 }
@@ -99,17 +128,58 @@ sub new {
                 when (/\G((?>true|false))\b/gc) {
                     return Sidef::Types::Bool::Bool->new($1), pos;
                 }
-                when (/\G(\d+)\b/gc) {
-                    return Sidef::Types::Number::Integer->new($1), pos;
-                }
-                when (/\G(\d+\.\d*)/gc) {
+                when (/\G([+-]?\d+\.\d*)/gc) {
                     return Sidef::Types::Number::Float->new($1), pos;
                 }
+                when (/\G([+-]?\d+)\b/gc) {
+                    return Sidef::Types::Number::Integer->new($1), pos;
+                }
                 default {
-                    die "Syntax error at line $line.\n";
+                    warn "Can't parse expression!\n";
+                    syntax_error(code => $_, pos => pos($_));
                 }
             }
         }
+    }
+
+    sub parse_arguments {
+        my %opt = @_;
+
+        my @arg;
+
+        given ($opt{'code'}) {
+            {
+
+                if (/\G/gc && defined(my $pos = parse_whitespace(code => substr($_, pos)))) {
+                    pos($_) = $pos + pos($_);
+                }
+
+                when (/\G\(/gc) {
+                    $parentheses++;
+                    redo;
+                }
+                when (/\G\)/gc) {
+                    $parentheses--;
+
+                    if ($parentheses == 0) {
+                        return \@arg, pos;
+                    }
+
+                    if ($parentheses < 0) {
+                        warn "Unbalanced parentheses!";
+                        syntax_error(code => $_, pos => pos($_));
+                    }
+
+                    redo;
+                }
+                default {
+                    my ($obj, $pos) = parse_script(code => substr($_, pos));
+                    return $obj, $pos;
+                }
+
+            }
+        }
+
     }
 
     sub parse_script {
@@ -117,11 +187,11 @@ sub new {
         my %opt = @_;
 
         my %struct;
-        my $ref = $struct{main} //= [];
         my $class = 'main';
 
         given ($opt{code}) {
             {
+
                 when (/\Gclass\h+/gc) {
                     when (/\G"(.*?)"/gc) {
                         $class = $1;
@@ -134,10 +204,11 @@ sub new {
 
                     die "Expected class name, at line $line.\n";
                 }
+
                 when (/\G\z/gc) {
                     return \%struct;
                 }
-                when (/\G->/gc) {
+                when (/\G->/gc || /\G(?=\s*$operators_re)/) {
 
                     my ($method_name, $pos) = get_method_name(code => substr($_, pos));
                     pos($_) = $pos + pos;
@@ -160,6 +231,42 @@ sub new {
 
                     #my ( $expr, $pos ) = parse_expr(code => substr($_, pos));
 
+                }
+
+                when (/\G(?=\()/) {
+                    my ($arg, $pos) = parse_arguments(code => substr($_, pos));
+                    pos($_) = $pos + pos;
+
+                    #push @{$struct{$class}[-1]{arg}}, $arg;
+                    push @{$struct{$class}[-1]{call}[-1]{arg}}, $arg;
+
+                    redo;
+                }
+
+                when (/\G\)/gc) {
+                    --$parentheses;
+
+                    if (@{[caller(1)]}) {
+                        return (\%struct, pos);
+                    }
+                    else {
+                        redo;
+                    }
+
+                    #say "->>>>", substr($_, pos);
+
+                    #if($parentheses == 0){
+                    #    return (\%struct, pos);
+                    #}
+                    #els
+                    #else{
+                    #    return \%struct, pos;
+                    #}
+
+                    # redo;
+                    #--$parentheses;
+
+                    #redo;
                 }
 
                 #while(/\G\h*,/gc){
@@ -186,15 +293,6 @@ sub new {
                         redo;
                     }
 
-                    # ++$ref;
-                    # $ref->[-1]
-
-                    #++$#{$struct{$class}};
-                    #$struct{$class}[-1]{self} = $obj;
-
-                    #redo;
-
-                    #push @{$struct{$class}}
                 }
             }
         }
@@ -264,13 +362,84 @@ my $code = <<'CODE';
 
 81->sqrt;
 
+#12/(63/(2)/(3))->to_s->print;
+
+24/(18*(3))->to_s(3)->print;   # should print 4
+
 "string"->lc->uc->print;
 
-#12/ ( 63/(2) /(3) )->to_s->print;
+
+44.2->int->log10;
 
 CODE
 
-my $struct = parse_script(code => $code);
+my ($struct, $pos) = parse_script(code => $code);
 
-use Data::Dumper;
-print Dumper $struct;
+use Data::Dump qw(pp);
+pp $struct;
+
+__END__
+
+BINGO!!!
+
+
+{
+  main => [
+    {
+      call => [{ name => "sqrt" }],
+      self => bless(do{\(my $o = 81)}, "Sidef::Types::Number::Integer"),
+    },
+    {
+      call => [
+                {
+                  arg  => [
+                            {
+                              main => [
+                                {
+                                  call => [
+                                            {
+                                              arg  => [
+                                                        {
+                                                          main => [
+                                                            {
+                                                              self => bless(do{\(my $o = 3)}, "Sidef::Types::Number::Integer"),
+                                                            },
+                                                          ],
+                                                        },
+                                                      ],
+                                              name => "*",
+                                            },
+                                          ],
+                                  self => bless(do{\(my $o = 18)}, "Sidef::Types::Number::Integer"),
+                                },
+                              ],
+                            },
+                          ],
+                  name => "/",
+                },
+                {
+                  arg  => [
+                            {
+                              main => [
+                                {
+                                  self => bless(do{\(my $o = 3)}, "Sidef::Types::Number::Integer"),
+                                },
+                              ],
+                            },
+                          ],
+                  name => "to_s",
+                },
+                { name => "print" },
+              ],
+      self => bless(do{\(my $o = 24)}, "Sidef::Types::Number::Integer"),
+    },
+    {
+      call => [{ name => "lc" }, { name => "uc" }, { name => "print" }],
+      self => bless(do{\(my $o = "string")}, "Sidef::Types::String::Double"),
+    },
+    {
+      call => [{ name => "int" }, { name => "log10" }],
+      self => bless(do{\(my $o = 44.2)}, "Sidef::Types::Number::Float"),
+    },
+  ],
+}
