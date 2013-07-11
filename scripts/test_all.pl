3708eabed70192734addf6aab74c78627d1bfa8e
Daniel Șuteu <trizenx@gmail.com>
2013-07-12 00:01:55 +0300

    Fixed the test_all.pl script.

--- scripts/test_all.pl
+++ scripts/test_all.pl
@@ -13,7 +13,7 @@ foreach my $sidef_script (glob '*.sf') {
     print "\n\n=>> Executing $sidef_script\n", "-" x 80, "\n";
     sleep 1;
 
-    system $^X, 'sidef', $sidef_script;
+    system $^X, '../bin/sidef', $sidef_script;
 
     if ($? != 0) {
         die "Non-zero exit code for script: $sidef_script\n";
