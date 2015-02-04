a212eeb0abfadf08240bdae5faace322ab003a62
Trizen <trizenx@gmail.com>
2015-02-04 14:15:40 +0200

    More progress on Sidef::Deparser;

--- lib/Sidef/Deparser.pm
+++ lib/Sidef/Deparser.pm
@@ -2,7 +2,7 @@ package Sidef::Deparser {
 
     use 5.014;
     our @ISA = qw(Sidef);
-    use Scalar::Util qw(refaddr);
+    use Scalar::Util qw(refaddr reftype);
 
     # This module is under development...
 
@@ -11,36 +11,73 @@ package Sidef::Deparser {
         bless \%opts, __PACKAGE__;
     }
 
+    sub _indent {
+        my $i = 4;
+        1 while (caller($i++));
+        ' ' x (2 * log($i) / log(2));    # needs work
+    }
+
+    sub _dump_vars {
+        my (@vars) = @_;
+        join(', ', map { $_->{name} } map { @{$_->{vars}} } @vars);
+    }
+
     my %addr;
 
     sub deparse_expr {
         my ($self, $expr) = @_;
 
-        my $self_obj = $expr->{self};
+        my $code = '';
+        my $obj  = $expr->{self};
 
         # Self obj
-        my $ref = ref($self_obj);
+        my $ref = ref($obj);
         if ($ref eq 'HASH') {
-            $self_obj = join(', ', $self->deparse($self_obj));
+            $code = join(', ', $self->deparse($obj));
         }
         elsif ($ref eq "Sidef::Variable::Variable") {
-            if ($self_obj->{type} eq 'func') {
-                if ($addr{refaddr($self_obj)}++) {
-                    $self_obj = $self_obj->{name};
+            if ($obj->{type} eq 'var') {
+                $code = $obj->{name};
+            }
+            elsif ($obj->{type} eq 'func') {
+                if ($addr{refaddr($obj)}++) {
+                    $code = $obj->{name};
                 }
                 else {
-                    $self_obj = ("func $self_obj->{name} " . $self->deparse_expr({self => $self_obj->{value}}));
+                    my $block = $obj->{value};
+                    $code = "func $obj->{name}";
+                    my $vars = delete $block->{init_vars};
+                    $code .= '(' . _dump_vars(@{$vars}[0 .. $#{$vars} - 1]) . ')';
+                    $code .= $self->deparse_expr({self => $block});
                 }
             }
         }
         elsif ($ref eq 'Sidef::Variable::Init') {
-            $self_obj = 'var(' . join(', ', map { $_->{name} } @{$self_obj->{vars}}) . ')';
+            $code = 'var(' . join(', ', map { $_->{name} } @{$obj->{vars}}) . ')';
         }
         elsif ($ref eq 'Sidef::Variable::Ref') {
-            $self_obj = '__some-var-ref__';
+            ## ok
+        }
+        elsif ($ref eq 'Sidef::Sys::Sys') {
+            $code = 'Sys';
         }
         elsif ($ref eq 'Sidef::Types::Block::Code') {
-            $self_obj = '{' . join(";\n", $self->deparse($self_obj->{code})) . '}';
+            $code = '{';
+            if (exists($obj->{init_vars}) and @{$obj->{init_vars}} > 1) {
+                my $vars = $obj->{init_vars};
+                $code .= '|' . _dump_vars(@{$vars}[0 .. $#{$vars} - 1]) . "|";
+            }
+            $code .= "\n";
+            my $space      = pop @{$self->{spaces}};
+            my @statements = $self->deparse($obj->{code});
+            my $sp         = pop @{$self->{spaces}};
+            $code .= $sp . join(";\n" . pop(@{$self->{spaces}}), @statements) . "\n" . $space . '}';
+        }
+        elsif ($ref eq 'Sidef::Types::Number::Number') {
+            $code = $obj->dump;
+        }
+        elsif (reftype($obj) eq 'SCALAR') {
+            $code = $obj->dump;
         }
 
         # Method call on the self obj (+optional arguments)
@@ -50,23 +87,27 @@ package Sidef::Deparser {
 
                 }
                 elsif ($call->{method} =~ /^[[:alpha:]_]/) {
-                    $self_obj .= ".$call->{method}";
+                    $code .= ".$call->{method}";
                 }
                 else {
-                    $self_obj .= "$call->{method}";
+                    $code .= "$call->{method}";
                 }
 
                 if (exists $call->{arg}) {
-                    $self_obj .= " +some-arguments";
+                    $code .= '('
+                      . join(', ', map { ref($_) eq 'HASH' ? $self->deparse($_) : $self->deparse_expr($_) } @{$call->{arg}})
+                      . ')';
                 }
             }
         }
 
-        $self_obj;
+        $code;
     }
 
     sub deparse {
         my ($self, $struct) = @_;
+
+        push @{$self->{spaces}}, _indent();
 
         my @results;
         foreach my $class (grep exists $struct->{$_}, @{$self->{namespaces}}, 'main') {
