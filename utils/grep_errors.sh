b77a6c475af7ccea9623f32417e451b323afcbc8
trizen <trizenx@gmail.com>
2015-05-29 22:06:20 +0300

    - Version bump to 0.05

--- utils/grep_errors.sh
+++ utils/grep_errors.sh
@@ -2,4 +2,4 @@
 
 perl -W ../bin/sidef -t $@ 2> /tmp/errors-$$.txt
 cat /tmp/errors-$$.txt | egrep '\b(?:Sidef|sidef)\b' | grep -v ' redefined at ' | grep -v '^Deep recursion ' | grep -v '^Prototype mismatch' | grep -v ' used only once '
-#rm /tmp/errors-$$.txt
+rm /tmp/errors-$$.txt
