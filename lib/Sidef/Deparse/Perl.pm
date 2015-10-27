a1e196414208239d45d6e12d22ae701fc896aa2b
trizen <trizenx@gmail.com>
2015-10-28 01:39:34 +0200

    - Minor bug fix for conditional return with postif 'if'
    Example:
    	func foo() {
    	   return 42 if true;		# works now
    	}
    
    - Added a few more scripts to the project Euler.

--- lib/Sidef/Deparse/Perl.pm
+++ lib/Sidef/Deparse/Perl.pm
@@ -1075,10 +1075,11 @@ HEADER
                 if ($ref eq 'Sidef::Types::Block::Return') {
 
                     if (exists $self->{function}) {
+                        $code .= 'do {';
                         if (@{$call->{arg}}) {
                             $code .= '@return = ' . $self->deparse_args(@{$call->{arg}}) . ';';
                         }
-                        $code .= 'goto ' . "END$self->{function}";
+                        $code .= 'goto ' . "END$self->{function}}";
                     }
                     else {
                         $code .= 'return Sidef::Types::Block::Return->new' . $self->deparse_args(@{$call->{arg}});
