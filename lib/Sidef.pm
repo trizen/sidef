3fa63b0bcd68aae92ce2a08d6f8f8c267bb3b34d
trizen <trizenx@gmail.com>
2016-02-08 03:57:17 +0200

    - Version 2.22 is here!

--- lib/Sidef.pm
+++ lib/Sidef.pm
@@ -1,7 +1,7 @@
 package Sidef {
 
     use 5.014;
-    our $VERSION = '2.21';
+    our $VERSION = '2.22';
 
     our $SPACES      = 0;    # the current number of spaces
     our $SPACES_INCR = 4;    # the number of spaces incrementor
