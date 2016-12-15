bebf042da8a3d7d497ef08b4bf02cc67e28c2d93
trizen <trizenx@gmail.com>
2016-12-15 23:16:31 +0200

    - Undefined methods from Fcntl are no longer created in the File class.

--- lib/Sidef/Types/Glob/File.pm
+++ lib/Sidef/Types/Glob/File.pm
@@ -33,7 +33,7 @@ package Sidef::Types::Glob::File {
 
         my %cache;
         foreach my $name (@Fcntl::EXPORT, @Fcntl::EXPORT_OK) {
-            $name =~ /^[a-z]/i or next;
+            ($name =~ /^[A-Z]/ and defined(&{'Fcntl::' . $name})) or next;
             *{__PACKAGE__ . '::' . $name} = sub {
                 $cache{$name} //= Sidef::Types::Number::Number->_set_uint(&{'Fcntl::' . $name});
             };
