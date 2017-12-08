7767e5de01108c20968dce86c03536254c5b1348
trizen <trizen@protonmail.com>
2017-12-08 13:40:41 +0200

    Version 3.10

--- lib/Sidef.pm
+++ lib/Sidef.pm
@@ -1,7 +1,7 @@
 package Sidef {
 
     use 5.014;
-    our $VERSION = '3.05';
+    our $VERSION = '3.10';
 
     our $SPACES      = 0;    # the current number of indentation spaces
     our $SPACES_INCR = 4;    # the number of indentation spaces
