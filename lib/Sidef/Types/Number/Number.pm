b00d9d337c05a6745e68f8665a3ccede149b7ab2
trizen <trizen@protonmail.com>
2018-08-28 20:45:52 +0300

    - Added the Number `is_frobenius_underwood_pseudoprime(n)` method.
    See also: https://arxiv.org/abs/1706.01265

--- lib/Sidef/Types/Number/Number.pm
+++ lib/Sidef/Types/Number/Number.pm
@@ -8315,6 +8315,16 @@ package Sidef::Types::Number::Number {
           : Sidef::Types::Bool::Bool::FALSE;
     }
 
+    sub is_frobenius_underwood_pseudoprime {
+        my ($n) = @_;
+        __is_int__($$n)
+          && Math::Prime::Util::GMP::is_frobenius_underwood_pseudoprime(_big2uistr($n)
+                                                                        // (return Sidef::Types::Bool::Bool::FALSE),
+                                                                       )
+          ? Sidef::Types::Bool::Bool::TRUE
+          : Sidef::Types::Bool::Bool::FALSE;
+    }
+
     sub is_prob_prime {
         my ($x, $k) = @_;
 
