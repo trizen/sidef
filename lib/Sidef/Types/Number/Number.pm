553184d3da6578faa42bff77ae0f56357df5733d
trizen <trizen@protonmail.com>
2018-08-28 20:16:42 +0300

    - Added the Number `is_strongish_lucas_pseudoprime(n)`, which is almost an extra strong Lucas primality test.
    Example:
    
    	say (0..50000 -> grep{ !.is_prime && .is_strongish_lucas_pseudoprime })
    
    ...produces the following list of pseudoprimes (<= 50000):
    
    	[989, 3239, 5777, 10469, 10877, 27971, 29681, 30739, 31631, 39059]

--- lib/Sidef/Types/Number/Number.pm
+++ lib/Sidef/Types/Number/Number.pm
@@ -8281,6 +8281,15 @@ package Sidef::Types::Number::Number {
 
     *is_extra_strong_lucas_pseudoprime = \&is_stronger_lucas_pseudoprime;
 
+    sub is_strongish_lucas_pseudoprime {
+        my ($n) = @_;
+        __is_int__($$n)
+          && Math::Prime::Util::GMP::is_almost_extra_strong_lucas_pseudoprime(_big2uistr($n)
+                                                                              // (return Sidef::Types::Bool::Bool::FALSE))
+          ? Sidef::Types::Bool::Bool::TRUE
+          : Sidef::Types::Bool::Bool::FALSE;
+    }
+
     sub is_prob_prime {
         my ($x, $k) = @_;
 
