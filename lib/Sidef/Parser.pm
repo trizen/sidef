5068296129ca27119b3cce4425f868ee38dc0e12
trizen <trizen@protonmail.com>
2019-08-11 00:22:55 +0300

    - Allow numbers to be separated by commas in `%n(...)` and `%i(...)` integer arrays (in addition to whitespace).
    Example (these are now all equivalent):
    
    	say %n(1 2 3 4)		# [1, 2, 3, 4]
    	say %n(1,2,3,4)		# [1, 2, 3, 4]
    	say %n(1, 2, 3, 4)	# [1, 2, 3, 4]

--- lib/Sidef/Parser.pm
+++ lib/Sidef/Parser.pm
@@ -2228,29 +2228,35 @@ package Sidef::Parser {
                 my $strings = $self->get_quoted_words(code => $opt{code});
 
                 if ($type eq 'w' or $type eq '<') {
-                    return Sidef::Types::Array::Array->new(
-                                                [map { Sidef::Types::String::String->new(s{\\(?=[\\#\s])}{}gr) } @{$strings}]);
+                    return Sidef::Types::Array::Array->new(map { Sidef::Types::String::String->new(s{\\(?=[\\#\s])}{}gr) }
+                                                           @{$strings});
                 }
-                elsif ($type eq 'i') {
-                    return Sidef::Types::Array::Array->new(
-                                           [map { Sidef::Types::Number::Number->new(s{\\(?=[\\#\s])}{}gr)->int } @{$strings}]);
+
+                if ($type eq 'i') {
+                    return
+                      Sidef::Types::Array::Array->new(map { Sidef::Types::Number::Number->new(s{\\(?=[\\#\s])}{}gr)->int }
+                                                        split(/[\s,]+/, join(' ', @$strings)));
                 }
-                elsif ($type eq 'n') {
-                    return Sidef::Types::Array::Array->new(
-                                                [map { Sidef::Types::Number::Number->new(s{\\(?=[\\#\s])}{}gr) } @{$strings}]);
+
+                if ($type eq 'n') {
+                    return
+                      Sidef::Types::Array::Array->new(map { Sidef::Types::Number::Number->new(s{\\(?=[\\#\s])}{}gr) }
+                                                        split(/[\s,]+/, join(' ', @$strings)));
                 }
-                elsif ($type eq 'v') {
+
+                if ($type eq 'v') {
                     return
                       Sidef::Types::Array::Vector->new(map { Sidef::Types::Number::Number->new(s{\\(?=[\\#\s])}{}gr) }
                                                          split(/[\s,]+/, join(' ', @$strings)));
                 }
-                elsif ($type eq 'm') {
+
+                if ($type eq 'm') {
                     my @matrix;
                     my $data = join(' ', @$strings);
                     foreach my $line (split(/\s*;\s*/, $data)) {
                         push @matrix,
-                          Sidef::Types::Array::Array->new(
-                                     [map { Sidef::Types::Number::Number->new(s{\\(?=[\\#\s])}{}gr) } split(/[\s,]+/, $line)]);
+                          Sidef::Types::Array::Array->new(map { Sidef::Types::Number::Number->new(s{\\(?=[\\#\s])}{}gr) }
+                                                            split(/[\s,]+/, $line));
                     }
                     return Sidef::Types::Array::Matrix->new(@matrix);
                 }
