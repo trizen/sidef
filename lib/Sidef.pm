f2c2eda6a964cd0f76b81cc80fb473dd1fad3381
trizen <trizen@protonmail.com>
2020-07-20 17:01:39 +0300

    Version 3.96

--- lib/Sidef.pm
+++ lib/Sidef.pm
@@ -3,7 +3,7 @@ package Sidef {
     use utf8;
     use 5.016;
 
-    our $VERSION = '3.95';
+    our $VERSION = '3.96';
 
     our $SPACES      = 0;    # the current number of indentation spaces
     our $SPACES_INCR = 4;    # the number of indentation spaces
