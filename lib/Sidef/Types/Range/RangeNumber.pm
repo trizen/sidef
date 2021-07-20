d0432430c0c6b2f4b87b14b0381cedc2c39d89e2
trizen <trizen@protonmail.com>
2021-07-20 17:08:21 +0300

    - Added the RangeNumber `composites(a..b)` method.
    Example:
    
    	say composites(50..100)		# composite integers in the range 50..100
    	say composites(1..50 `by` 2)	# odd composite integers <= 50

--- lib/Sidef/Types/Range/RangeNumber.pm
+++ lib/Sidef/Types/Range/RangeNumber.pm
@@ -323,6 +323,24 @@ package Sidef::Types::Range::RangeNumber {
         $self->grep(Sidef::Types::Block::Block->new(code => sub { $_[0]->is_prime }));
     }
 
+    sub composites {
+        my ($self) = @_;
+
+        if ($self->{step}->abs->is_one) {
+
+            my $left  = Sidef::Types::Number::Number->new($self->{from});
+            my $right = Sidef::Types::Number::Number->new($self->{to});
+
+            if ($self->{step}->is_neg) {
+                return Sidef::Types::Number::Number::composites($right, $left)->flip;
+            }
+
+            return Sidef::Types::Number::Number::composites($left, $right);
+        }
+
+        $self->grep(Sidef::Types::Block::Block->new(code => sub { $_[0]->is_composite }));
+    }
+
     sub squarefree {
         my ($self) = @_;
 
