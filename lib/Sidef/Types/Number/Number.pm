d3108ebf0f6617ce000845970a627344b6bf8c95
trizen <trizen@protonmail.com>
2022-11-23 21:11:02 +0200

    - Added the Number `k.each_squarefree_strong_fermat_psp(base, from, upto, { ... })` method.
    Iterates over the squarefree strong Fermat pseudoprimes in the given range that have exactly `k` prime factors.
    
    When `upto` is not given, it iterates the range `2..from`.
    
    Example:
    
    	3.each_squarefree_strong_fermat_psp(2, 1e6, {|n| say n })          # iterate over squarefree strong 3-Fermat psp to base 2 <= 1e6
    	4.each_squarefree_strong_fermat_psp(2, 1e7, 1e8, {|n| say n })     # iterate over squarefree strong 4-Fermat psp to base 2 in range 1e7..1e8

--- lib/Sidef/Types/Number/Number.pm
+++ lib/Sidef/Types/Number/Number.pm
@@ -24275,7 +24275,7 @@ package Sidef::Types::Number::Number {
             $step = 5e7;
         }
 
-        if ($k > 4 and $step < 1e8) {
+        if ($k >= 5 and $step < 1e8) {
             $step = '1' . ('0' x 8);
         }
 
@@ -24390,7 +24390,7 @@ package Sidef::Types::Number::Number {
             $step = 5e7;
         }
 
-        if ($k > 4 and $step < 1e8) {
+        if ($k >= 5 and $step < 1e8) {
             $step = '1' . ('0' x 8);
         }
 
@@ -24443,7 +24443,7 @@ package Sidef::Types::Number::Number {
             $step = 5e6;
         }
 
-        if ($k > 4 and $step < 1e7) {
+        if ($k >= 5 and $step < 1e7) {
             $step = '1' . ('0' x 7);
         }
 
@@ -24457,6 +24457,67 @@ package Sidef::Types::Number::Number {
     }
 
     *each_squarefree_fermat_psp = \&squarefree_fermat_psp_each;
+
+    sub squarefree_strong_fermat_psp_each {
+        my ($k, $base, $from, $to, $block) = @_;
+
+        _valid(\$base, \$from);
+
+        if (defined($block)) {
+            _valid(\$to);
+            $from = _any2mpz($$from) // return ZERO;
+            $to   = _any2mpz($$to)   // return ZERO;
+        }
+        else {
+            $block = $to;
+            $to    = _any2mpz($$from) // return ZERO;
+            $from  = $ONE;
+        }
+
+        $k    = _any2ui($$k)    // return ZERO;
+        $base = _any2ui($$base) // return ZERO;
+
+        if (Math::GMPz::Rmpz_sgn($from) <= 0) {
+            $from = $ONE;
+        }
+
+        if ($k < 2) {
+            return ZERO;
+        }
+
+        # TODO: tweak the step value for better performance
+        my $step = Math::Prime::Util::GMP::vecprod(($k) x ($k + 1), Math::Prime::Util::GMP::pn_primorial($k));
+
+        if ($step < 1e7) {
+            $step = '1' . ('0' x 7);
+        }
+
+        if ($k == 2) {
+            $step = '1' . ('0' x 6);
+        }
+
+        if ($k == 4) {
+            $step = '5' . ('0' x 8);
+        }
+
+        if ($k >= 5 and $step < 5e9) {
+            $step = '5' . ('0' x 9);
+        }
+
+        if ($k >= 6 and $step < 2e11) {
+            $step = '2' . ('0' x 11);
+        }
+
+        if ($step > ULONG_MAX) {
+            $step = Math::GMPz::Rmpz_init_set_str("$step", 10);
+        }
+
+        _generic_each($from, $to, $block,
+                      sub { $step },
+                      sub { _sieve_almost_primes($_[0], $_[1], $k, squarefree => 1, strong => 1, fermat => $base) });
+    }
+
+    *each_squarefree_strong_fermat_psp = \&squarefree_strong_fermat_psp_each;
 
     sub fermat_psp_each {
         my ($k, $base, $from, $to, $block) = @_;
@@ -24493,10 +24554,10 @@ package Sidef::Types::Number::Number {
         }
 
         if ($k == 4) {
-            $step = 5e6;
+            $step = '5' . ('0' x 6);
         }
 
-        if ($k > 4 and $step < 1e7) {
+        if ($k >= 5 and $step < 1e7) {
             $step = '1' . ('0' x 7);
         }
 
