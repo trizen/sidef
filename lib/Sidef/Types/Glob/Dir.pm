cc3570dd9f524b94af57522c31a520c34b3b6e0f
trizen <trizen@protonmail.com>
2023-02-19 21:25:12 +0200

    - Added the Dir `.find` method.
    Browses a given directory recursively and returns an array with File and Dir objects.
    
    Example:
    
    	say Dir.tmp.find
    
    It also takes an optional block argument, which is called for each file.
    
    	Dir.tmp.find {|file|
    	    say file
    	}
    
    Also aliased as `.browse`.

--- lib/Sidef/Types/Glob/Dir.pm
+++ lib/Sidef/Types/Glob/Dir.pm
@@ -5,7 +5,7 @@ package Sidef::Types::Glob::Dir {
 
     use parent qw(
       Sidef::Types::Glob::File
-      );
+    );
 
     require Encode;
     require File::Spec;
@@ -51,6 +51,35 @@ package Sidef::Types::Glob::Dir {
     }
 
     *temp = \&tmp;
+
+    sub find {
+        my ($self, $arg) = @_;
+        state $x = require File::Find;
+
+        my @files;
+        my $ref      = ref($arg // '');
+        my $is_block = $ref eq 'Sidef::Types::Block::Block';
+
+        File::Find::find(
+            sub {
+                my $file = Encode::decode_utf8($File::Find::name);
+
+                if (-d $file) {
+                    $file = Sidef::Types::Glob::Dir->new($file);
+                }
+                else {
+                    $file = Sidef::Types::Glob::File->new($file);
+                }
+
+                $is_block ? $arg->run($file) : push(@files, $file);
+            },
+            $$self
+        );
+
+        $is_block ? $self : Sidef::Types::Array::Array->new(\@files);
+    }
+
+    *browse = \&find;
 
     sub cwd {
         state $x = require Cwd;
