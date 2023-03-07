trizen/sidef

commit 14ae1a3adb35c36b21b5d965c2145140c837921f

- Optimized Number `is_lucas_carmichael(n)` for native `n`.

lib/Sidef/Types/Number/Number.pm
@@ -23897,22 +23897,12 @@ package Sidef::Types::Number::Number {
                             }
 #>>>
 
-                            if ($fermat and $m == 1) {
-                                for (my $v = $p * $p ; $v - 1 < $B ; $v *= $p) {
-                                    if ($v >= $A) {
+                            for (my $v = (($fermat and $m == 1) ? ($p * $p) : ($m * $p)) ; $v - 1 < $B ; $v *= $p) {
+                                if ($v >= $A) {
+                                    if ($fermat) {
                                         Math::Prime::Util::powmod($fermat, $v - 1, $v) == 1 or last;
-                                        push @omega_primes, $v;
                                     }
-                                }
-                            }
-                            else {
-                                for (my $v = $m * $p ; $v - 1 < $B ; $v *= $p) {
-                                    if ($v >= $A) {
-                                        if ($fermat) {
-                                            Math::Prime::Util::powmod($fermat, $v - 1, $v) == 1 or last;
-                                        }
-                                        push @omega_primes, $v;
-                                    }
+                                    push @omega_primes, $v;
                                 }
                             }
                         }
@@ -24076,48 +24066,30 @@ package Sidef::Types::Number::Number {
 
                             if ($fermat and Math::GMPz::Rmpz_cmp_ui($m, 1) == 0) {
                                 Math::GMPz::Rmpz_set_ui($v, $p);
-                                for (Math::GMPz::Rmpz_mul_ui($v, $v, $p) ;
-                                     Math::GMPz::Rmpz_cmp($v, $B) <= 0 ;
-                                     Math::GMPz::Rmpz_mul_ui($v, $v, $p)) {
+                            }
+                            else {
+                                Math::GMPz::Rmpz_set($v, $m);
+                            }
 
-                                    if (Math::GMPz::Rmpz_cmp($v, $A) >= 0) {
+                            for (Math::GMPz::Rmpz_mul_ui($v, $v, $p) ;
+                                 Math::GMPz::Rmpz_cmp($v, $B) <= 0 ;
+                                 Math::GMPz::Rmpz_mul_ui($v, $v, $p)) {
 
+                                if (Math::GMPz::Rmpz_cmp($v, $A) >= 0) {
+
+                                    if ($fermat) {
                                         Math::GMPz::Rmpz_sub_ui($u, $v, 1);
                                         Math::GMPz::Rmpz_powm($u, $w, $u, $v);
                                         Math::GMPz::Rmpz_cmp_ui($u, 1) == 0 or last;
-
-                                        my $value =
-                                            Math::GMPz::Rmpz_fits_ulong_p($v)
-                                          ? Math::GMPz::Rmpz_get_ui($v)
-                                          : Math::GMPz::Rmpz_init_set($v);
-
-                                        if (!$seen{$value}++) {
-                                            push @omega_primes, $value;
-                                        }
                                     }
-                                }
-                            }
-                            else {
-                                for (Math::GMPz::Rmpz_mul_ui($v, $m, $p) ;
-                                     Math::GMPz::Rmpz_cmp($v, $B) <= 0 ;
-                                     Math::GMPz::Rmpz_mul_ui($v, $v, $p)) {
 
-                                    if (Math::GMPz::Rmpz_cmp($v, $A) >= 0) {
+                                    my $value =
+                                        Math::GMPz::Rmpz_fits_ulong_p($v)
+                                      ? Math::GMPz::Rmpz_get_ui($v)
+                                      : Math::GMPz::Rmpz_init_set($v);
 
-                                        if ($fermat) {
-                                            Math::GMPz::Rmpz_sub_ui($u, $v, 1);
-                                            Math::GMPz::Rmpz_powm($u, $w, $u, $v);
-                                            Math::GMPz::Rmpz_cmp_ui($u, 1) == 0 or last;
-                                        }
-
-                                        my $value =
-                                            Math::GMPz::Rmpz_fits_ulong_p($v)
-                                          ? Math::GMPz::Rmpz_get_ui($v)
-                                          : Math::GMPz::Rmpz_init_set($v);
-
-                                        if ($fermat ? (!$seen{$value}++) : 1) {
-                                            push @omega_primes, $value;
-                                        }
+                                    if ($fermat ? (!$seen{$value}++) : 1) {
+                                        push @omega_primes, $value;
                                     }
                                 }
                             }
@@ -26384,8 +26356,8 @@ package Sidef::Types::Number::Number {
         $n = $$n;
 
         # If n is a native integer, Math::Prime::Util::is_carmichael() is slighly faster.
-        if (_fits_ulong($n)) {
-            $n = _get_ulong($n);
+        if (!ref($n) or _fits_ulong($n)) {
+            $n = _get_ulong($n) if ref($n);
             return (
                     (HAS_PRIME_UTIL ? Math::Prime::Util::is_carmichael($n) : Math::Prime::Util::GMP::is_carmichael($n))
                     ? Sidef::Types::Bool::Bool::TRUE
@@ -26583,6 +26555,33 @@ package Sidef::Types::Number::Number {
         my ($n) = @_;
 
         $n = $$n;
+
+        if (!ref($n) and $n < ULONG_MAX) {
+
+            $n >= 399   or return Sidef::Types::Bool::Bool::FALSE;
+            $n % 2 == 1 or return Sidef::Types::Bool::Bool::FALSE;
+
+            foreach my $p (3, 5, 7, 11, 13, 17, 19) {
+                if ($n % $p == 0) {
+                    if ($n % ($p * $p) == 0) {
+                        return Sidef::Types::Bool::Bool::FALSE;
+                    }
+                    ($n + 1) % ($p + 1) == 0
+                      or return Sidef::Types::Bool::Bool::FALSE;
+                }
+            }
+
+            if (HAS_PRIME_UTIL) {
+                my %seen;
+                foreach my $p (Math::Prime::Util::factor($n)) {
+                    $seen{$p}++
+                      and return Sidef::Types::Bool::Bool::FALSE;
+                    ($n + 1) % ($p + 1) == 0
+                      or return Sidef::Types::Bool::Bool::FALSE;
+                }
+                return return Sidef::Types::Bool::Bool::TRUE;
+            }
+        }
 
         if (ref($n) ne 'Math::GMPz') {
             __is_int__($n) || return Sidef::Types::Bool::Bool::FALSE;

scripts/Tests/number_methods.sf
@@ -2,6 +2,10 @@
 
 # Tests for some Number methods.
 
+# Timings:
+#   4 March 2023: 4.791s (with MPU)
+#   4 March 2023: 6.789s (without MPU)
+
 assert_eq(squarefree_sum(1e6), 303961062910)
 assert_eq(squarefree_sum(1e7), 30396557311887)
 

commit 522e4ba951761aa6cc7fd488b1da8fa9e43e6f8b

Fixed an issue Number `is_lucas_carmichael(n)` for native integers `n`.
It incorrectly reported prime numbers as Lucas-Carmichael numbers.

Issue introduced in: 14ae1a3adb35c36b21b5d965c2145140c837921f

lib/Sidef/Types/Number/Number.pm
@@ -26572,14 +26572,21 @@ package Sidef::Types::Number::Number {
             }
 
             if (HAS_PRIME_UTIL) {
+
+                my @factors = Math::Prime::Util::factor($n);
+
+                if (scalar(@factors) <= 2) {
+                    return Sidef::Types::Bool::Bool::FALSE;
+                }
+
                 my %seen;
-                foreach my $p (Math::Prime::Util::factor($n)) {
+                foreach my $p (@factors) {
                     $seen{$p}++
                       and return Sidef::Types::Bool::Bool::FALSE;
                     ($n + 1) % ($p + 1) == 0
                       or return Sidef::Types::Bool::Bool::FALSE;
                 }
-                return return Sidef::Types::Bool::Bool::TRUE;
+                return Sidef::Types::Bool::Bool::TRUE;
             }
         }
 

scripts/Tests/number_methods.sf
@@ -3,8 +3,8 @@
 # Tests for some Number methods.
 
 # Timings:
-#   4 March 2023: 4.791s (with MPU)
-#   4 March 2023: 6.789s (without MPU)
+#   07 March 2023: 4.140s (with MPU)
+#   07 March 2023: 7.661s (without MPU)
 
 assert_eq(squarefree_sum(1e6), 303961062910)
 assert_eq(squarefree_sum(1e7), 30396557311887)
@@ -804,5 +804,53 @@ for name,f,g in ([
         }
     }
 }
+
+assert_eq(
+    20.of { .phi },
+    %n[0, 1, 1, 2, 2, 4, 2, 6, 4, 6, 4, 10, 4, 12, 6, 8, 8, 16, 6, 18]
+)
+
+assert_eq(
+    20.of { .lambda },
+    %n[0, 1, 1, 2, 2, 4, 2, 6, 2, 6, 4, 10, 2, 12, 6, 4, 4, 16, 6, 18]
+)
+
+assert_eq(
+    20.of { .liouville },
+    %n[-1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, -1, -1, -1, 1, 1, 1, -1, -1, -1]
+)
+
+assert_eq(
+    20.of { .moebius },
+    %n[0, 1, -1, -1, 0, -1, 1, -1, 0, 0, 1, -1, 0, -1, 1, 1, 0, -1, 0, -1]
+)
+
+assert_eq(
+    30.of { .bell },
+    %n[1, 1, 2, 5, 15, 52, 203, 877, 4140, 21147, 115975, 678570, 4213597, 27644437, 190899322, 1382958545, 10480142147, 82864869804, 682076806159, 5832742205057, 51724158235372, 474869816156751, 4506715738447323, 44152005855084346, 445958869294805289, 4638590332229999353, 49631246523618756274, 545717047936059989389, 6160539404599934652455, 71339801938860275191172],
+)
+
+assert_eq(
+    30.of { .bellmod(4171) },
+    %n[1, 1, 2, 5, 15, 52, 203, 877, 4140, 21147, 115975, 678570, 4213597, 27644437, 190899322, 1382958545, 10480142147, 82864869804, 682076806159, 5832742205057, 51724158235372, 474869816156751, 4506715738447323, 44152005855084346, 445958869294805289, 4638590332229999353, 49631246523618756274, 545717047936059989389, 6160539404599934652455, 71339801938860275191172].map { .mod(4171) },
+)
+
+assert_eq(
+    20.by { .is_composite },
+    %n[4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, 32],
+)
+
+assert_eq(20.of { .composite_count },   20.of { .is_composite ? 1 : 0 }.acc)
+assert_eq(20.of { .mertens },           20.of { .moebius }.acc)
+assert_eq(20.of { .pi },                20.of { .is_prime ? 1 : 0 }.acc)
+assert_eq(20.of { .sum_primes },        20.of { .is_prime ? _ : 0 }.acc)
+assert_eq(20.of { sum_primes(0,_) },    20.of { .is_prime ? _ : 0 }.acc)
+assert_eq(20.of { sum_primes(0,_,2) },  20.of { .is_prime ? _*_ : 0 }.acc)
+assert_eq(20.of { .sigma0 },            20.of { .divisors.len })
+assert_eq(20.of { .sigma(0) },          20.of { .divisors.len })
+assert_eq(20.of { .sigma },             20.of { .divisors.sum })
+assert_eq(20.of { .sigma(2) },          20.of { .divisors.sum { _*_ } })
+assert_eq(20.of { .omega },             20.of { .factor_exp.len })
+assert_eq(20.of { .bigomega },          20.of { .factor.len })
 
 say "** Tests passed!"

scripts/Tests/pseudoprimes.sf
@@ -2,6 +2,10 @@
 
 # Tests for some pseudoprime-related functions
 
+# Timings:
+#   07 March 2023: 2.403s (with MPU)
+#   07 March 2023: 3.420s (without MPU)
+
 assert_eq(
     10.by{.is_fermat_psp(9) && .is_composite },
     %n[4, 8, 28, 52, 91, 121, 205, 286, 364, 511],
@@ -17,6 +21,93 @@ with (%n[1373653, 1530787, 1987021, 2284453, 3116107, 5173601, 6787327, 11541307
     assert_eq(arr.grep { .is_psp(2,3,5) }, arr.grep { powmod(5, .dec, _) == 1 })
     assert_eq(arr.grep { .is_strong_psp(2,3,5) }, %n[25326001])
 }
+
+assert_eq(
+    10.by { .is_lucas_carmichael },
+    %n[399, 935, 2015, 2915, 4991, 5719, 7055, 8855, 12719, 18095],
+)
+
+assert_eq(
+    10.by { .is_carmichael },
+    %n[561, 1105, 1729, 2465, 2821, 6601, 8911, 10585, 15841, 29341],
+)
+
+assert_eq(
+    10.by { .is_psp && .is_composite },
+    %n[341, 561, 645, 1105, 1387, 1729, 1905, 2047, 2465, 2701]
+)
+
+assert_eq(
+    10.by { .is_psp(3) && .is_composite },
+    %n[91, 121, 286, 671, 703, 949, 1105, 1541, 1729, 1891]
+)
+
+assert_eq(
+    10.by { .is_psp(3) && .is_composite },
+    %n[91, 121, 286, 671, 703, 949, 1105, 1541, 1729, 1891]
+)
+
+assert_eq(
+    10.by { .is_psp(2,3) && .is_composite },
+    %n[1105, 1729, 2465, 2701, 2821, 6601, 8911, 10585, 15841, 18721]
+)
+
+assert_eq(
+    5.by { .is_strong_psp && .is_composite },
+    %n[2047, 3277, 4033, 4681, 8321]
+)
+
+assert_eq(
+    5.by { .is_strong_psp(3) && .is_composite },
+    %n[121, 703, 1891, 3281, 8401]
+)
+
+assert(1373653.is_strong_psp(2,3))
+
+assert_eq(
+    10.by{.is_super_psp && .is_composite},
+    %n[341, 1387, 2047, 2701, 3277, 4033, 4369, 4681, 5461, 7957]
+)
+
+assert_eq(
+    10.by{.is_super_psp(3) && .is_composite},
+    %n[91, 121, 671, 703, 949, 1541, 1891, 2701, 3281, 7381]
+)
+
+assert_eq(
+    3.by{.is_super_psp(2,3) && .is_composite},
+    %n[2701, 18721, 31621]
+)
+
+assert_eq(
+    3.by{.is_over_psp && .is_composite},
+    %n[2047, 3277, 4033]
+)
+
+assert_eq(
+    3.by{.is_over_psp(3) && .is_composite},
+    %n[121, 703, 3281]
+)
+
+assert_eq(
+    10.by{.is_fib_psp && .is_composite},
+    %n[323, 377, 1891, 3827, 4181, 5777, 6601, 6721, 8149, 10877]
+)
+
+assert_eq(
+    10.by{.is_pell_psp && .is_composite},
+    %n[169, 385, 741, 961, 1121, 2001, 3827, 4879, 5719, 6215]
+)
+
+assert_eq(
+    10.by{.is_lucas_psp && .is_composite},
+    %n[323, 377, 1159, 1829, 3827, 5459, 5777, 9071, 9179, 10877]
+)
+
+assert_eq(
+    5.by{.is_strong_lucas_psp && .is_composite},
+    %n[5459, 5777, 10877, 16109, 18971]
+)
 
 assert_eq(
     9.lucas_carmichael(61044448782239)