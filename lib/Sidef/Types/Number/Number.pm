44c3723fcba0d07c4f22a14447b05a39a3c622de
trizen <trizen@protonmail.com>
2023-08-20 12:25:50 +0300

    - Added the Number `n.prev_omega_prime(k=2)` method.
    Returns the previous k-omega prime smaller than `n`.
    
    Example:
    
    	say 1e6.prev_omega_prime(3)		#=> 999998
    	say 1e13.prev_omega_prime(7)		#=> 9999999999962
    
    Also works for arbitary large n and k:
    
    	say prev_omega_prime(2**128, 26)	#=> 339677285219770746148118429022712770870 (takes 50s)

--- lib/Sidef/Types/Number/Number.pm
+++ lib/Sidef/Types/Number/Number.pm
@@ -16273,7 +16273,7 @@ package Sidef::Types::Number::Number {
         }
 
         my $n_obj = $n;
-        my $k_obj = _set_int($k);
+        my $k_obj = bless \$k;
 
         $n = _any2mpz($$n) // goto &nan;
 
@@ -16312,6 +16312,65 @@ package Sidef::Types::Number::Number {
         }
 
         $k_obj->omega_prime_count($n_obj)->inc->nth_omega_prime($k_obj);
+    }
+
+    sub prev_omega_prime {
+        my ($n, $k) = @_;
+
+        if (defined($k)) {
+            _valid(\$k);
+            $k = _any2ui($$k) || goto &nan;
+        }
+        else {
+            $k = 2;
+        }
+
+        if ($k == 1) {
+            return $n->prev_prime_power;
+        }
+
+        my $n_obj = $n;
+        my $k_obj = bless \$k;
+
+        $n = _any2mpz($$n) // goto &nan;
+
+        if (Math::GMPz::Rmpz_sgn($n) < 0) {
+            goto &nan;
+        }
+
+        my $r = Math::GMPz::Rmpz_init_set(_cached_pn_primorial($k));
+
+        # The smallest k-omega prime is primorial(p_k)
+        if (Math::GMPz::Rmpz_cmp($n, $r) <= 0) {
+            goto &nan;
+        }
+
+        # TODO: detect large n with moderately large k
+        if ($k <= 7) {
+
+            # Optimization for native integers
+            if (HAS_NEW_PRIME_UTIL and Math::GMPz::Rmpz_fits_ulong_p($n)) {
+                $n = Math::GMPz::Rmpz_get_ui($n) - 1;
+                until (Math::Prime::Util::is_omega_prime($k, $n)) {
+                    --$n;
+                }
+                return bless \$n;
+            }
+
+            Math::GMPz::Rmpz_sub_ui($r, $n, 1);
+
+            my $r_obj = bless \$r;
+
+            until ($r_obj->is_omega_prime($k_obj)) {
+                Math::GMPz::Rmpz_sub_ui($r, $r, 1);
+            }
+
+            return $r_obj;
+        }
+
+        my $count = $k_obj->omega_prime_count($n_obj);
+        $count = $count->dec if $n_obj->is_omega_prime($k_obj);
+        $count->nth_omega_prime($k_obj);
     }
 
     sub nth_squarefree_almost_prime {
@@ -30064,7 +30123,9 @@ package Sidef::Types::Number::Number {
             return $n->iroot($k)->dec->ipow($k);
         }
 
-        $n->perfect_power_count->dec->nth_perfect_power;
+        my $count = $n->perfect_power_count;
+        $count = $count->dec if $n->is_perfect_power;
+        $count->nth_perfect_power;
     }
 
     sub is_power_of {
